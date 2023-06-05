f63b196bc5c41cf3abaaf5196cf36d548578275e
MunsMan <munsman.github@gmail.com>
2023-06-05 19:35:17 +0200

    [feat] adding a returner function for use lambdas

--- src/functions/aws/awsReturner/handler.ts
+++ src/functions/aws/awsReturner/handler.ts
@@ -1,7 +1,23 @@
+import fetch from 'node-fetch';
+const SERVICE_URL = process.env.SERVICE_URL;
+
 const awsReturner = async (event: any) => {
-    console.log(event)
-    console.log('Framework Metadata:', event.requestPayload.metadata)
-    console.log('Return Value:', event.responsePayload)
+    const { uFunctionId, executionStart, executionId, pregion } = event.requestPayload.metadata;
+    const executionEnd = Date.now();
+    const response = await fetch(`${SERVICE_URL}/return/${uFunctionId}`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            result: event.responsePayload,
+            executionStart,
+            executionEnd,
+            executionId,
+            pregion,
+        })
+    })
+    console.log(response)
 };
 
 export const main = awsReturner;
