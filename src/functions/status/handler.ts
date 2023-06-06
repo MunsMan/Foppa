5075460daf16796a051da4dbba7af6c267be6995
MunsMan <munsman.github@gmail.com>
2023-06-06 14:12:30 +0200

    [feat] adding error handling if log does not exist

--- src/functions/status/handler.ts
+++ src/functions/status/handler.ts
@@ -3,24 +3,25 @@ import { getLog, LogEventTypes } from "@libs/s3";
 
 const status: ValidatedEventAPIGatewayProxyEvent<null> = async (event) => {
     const { username, functionId, executionId } = event.pathParameters;
-    const log = await getLog('foppa-logs', { username, functionId, executionId });
-    const status = {
-        status: 'unknown',
-        steps: { done: -1, from: LogEventTypes.length - 1 }
-    }
-    if (!log) {
+    try {
+        const log = await getLog('foppa-logs', { username, functionId, executionId });
+        const status = {
+            status: 'unknown',
+            steps: { done: -1, from: LogEventTypes.length - 1 }
+        }
+        for (const event of LogEventTypes) {
+            console.log(event)
+            if (!Object.keys(log).includes(event)) break
+            status.steps.done++;
+            status.status = LogEventTypes[status.steps.done]
+        }
+        if (status.status === 'finished') {
+            status.payload = log.finished.response.result
+        }
+        return formatJSONResponse(status)
+    } catch {
         return formatJSONResponse({ status: 'unknown' }, 400)
     }
-    for (const event of LogEventTypes) {
-        console.log(event)
-        if (!Object.keys(log).includes(event)) break
-        status.steps.done++;
-        status.status = LogEventTypes[status.steps.done]
-    }
-    if (status.status === 'finished') {
-        status.payload = log.finished.response.result
-    }
-    return formatJSONResponse(status)
 };
 
 export const main = status;
