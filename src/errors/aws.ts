ebbc8029ea1b03dc39ef94bd779547faff9ae05f
MunsMan <munsman.github@gmail.com>
2023-06-06 14:11:57 +0200

    [feat] upgrading to throws

--- src/errors/aws.ts
+++ src/errors/aws.ts
@@ -1,21 +1,15 @@
 import type { LogGroup } from "@aws-sdk/client-cloudwatch-logs";
 
 
-const exit = (code: number = 1): never => {
-    process.exit(code)
-}
-
 export const unableToFindLogGroup = (logGroups: LogGroup[]): never => {
     if (logGroups.length === 0) {
-        console.error('ERROR: Found no Log Groups!')
+        throw new Error('ERROR: Found no Log Groups!')
     } else {
-        console.error(`ERROR: Found multiple Log Groups!\n${logGroups.map(lg => lg.logGroupName).join(' ')}`)
+        throw new Error(`ERROR: Found multiple Log Groups!\n${logGroups.map(lg => lg.logGroupName).join(' ')}`)
     }
-    return exit()
 }
 
 
 export const LogNotFound = (logIdentifier: LogIdentifier): never => {
-    console.log(`ERROR: Log not Found!\n${logIdentifier}`)
-    return exit()
+    throw new Error(`ERROR: Log not Found!\n${logIdentifier}`)
 }
