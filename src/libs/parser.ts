c21b3a8a41547c4983e1aa08fdf942fc055f84b3
MunsMan <munsman.github@gmail.com>
2023-07-24 15:04:54 +0200

    [feat] adding custom parse to ensure universal behaivor

--- src/libs/parser.ts
+++ src/libs/parser.ts
@@ -6,3 +6,17 @@ export const parseUFunctionId = (uFunctionId: string): [string, string] => {
     }
     throw `ERROR: unable this string as a uFunctionId ${uFunctionId}`
 }
+
+
+
+
+export const toPRegion = (provider: CloudProvider, region: string) => (
+    `${provider}/${region}`
+)
+
+export const parsePRegion = (pregion: string): [CloudProvider, string] => {
+    if (pregion.search('/')) {
+        return pregion.split('/', 2) as [CloudProvider, string]
+    }
+    throw `ERROR: unable to parse this string {${pregion}} as a pregion `
+} 
