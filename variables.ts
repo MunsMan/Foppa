165e1d05d630a7a24d009c830c00d42bce4abaaa
MunsMan <munsman.github@gmail.com>
2023-07-24 15:07:24 +0200

    [var] adding global variable for code bucket

--- variables.ts
+++ variables.ts
@@ -3,7 +3,8 @@ const variables = {
     OPTIMIZATION_REQUEST_TOPIC_ARN: 'arn:aws:sns:us-east-1:807699729275:OptimizationRequests',
     RUN_REQUEST_TOPIC_ARN: 'arn:aws:sns:us-east-1:807699729275:RunRequest',
     FUNCTION_EXECUTION_COUNTER_ARN: 'arn:aws:dynamodb:us-east-1:807699729275:table/FunctionExecutionCounter',
-    LOGGING_BUCKET_NAME: 'foppa-logs'
+    LOGGING_BUCKET_NAME: 'foppa-logs',
+    FOPPA_BUCKET_NAME: 'foppa'
 } as const;
 
 export default variables;
