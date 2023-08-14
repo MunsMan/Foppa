40f4e07eb0e6b8e9b59ea6ad2997cea5ff993946
MunsMan <munsman.github@gmail.com>
2023-08-14 13:46:41 +0200

    [refa] updating the RegionRunner Parameters

--- src/libs/dynamodb.ts
+++ src/libs/dynamodb.ts
@@ -1,4 +1,4 @@
-import { DynamoDBClient, GetItemCommand, PutItemCommand, UpdateItemCommand, UpdateItemCommandInput } from "@aws-sdk/client-dynamodb";
+import { DynamoDBClient, GetItemCommand, PutItemCommand, QueryCommand, QueryCommandInput, UpdateItemCommand, UpdateItemCommandInput } from "@aws-sdk/client-dynamodb";
 import { marshall, unmarshall } from "@aws-sdk/util-dynamodb"
 import type { GetItemCommandInput, PutItemCommandInput } from "@aws-sdk/client-dynamodb";
 import variables from "variables";
@@ -19,6 +19,24 @@ export const getValue = async <T>(table: DBTables, params: QueryParams): Promise
     return unmarshall(response.Item) as T
 }
 
+export const queryValues = async<T>(table: DBTables, params: QueryParams): Promise<T[]> => {
+    const variables = {}
+    Object.values(params).map((value, index) => {
+        const res = {};
+        res[typeof value === 'string' ? 'S' : 'N'] = value
+        variables[`:v${index + 1}`] = res
+    })
+    const query = Object.keys(params).map((key, index) => (`${key} = :v${index + 1} `)).join(' AND ');
+    console.log(variables)
+    const input: QueryCommandInput = {
+        TableName: table,
+        KeyConditionExpression: query,
+        ExpressionAttributeValues: variables
+    }
+    const response = await db.send(new QueryCommand(input))
+    return (response.Items ?? []).map((item) => (unmarshall(item) as T))
+}
+
 export const putValue = async (table: DBTables, item: QueryParams) => {
     const input: PutItemCommandInput = {
         TableName: table,
@@ -34,6 +52,7 @@ export const putValue = async (table: DBTables, item: QueryParams) => {
 type ItemQueryParams<T extends DBTables> =
     T extends 'RegionExecutionCounter' ? { uFunctionId: string, pregion: string } :
     T extends 'FunctionExecutionCounter' ? { username: string, functionId: string } :
+    T extends 'RegionRunnerURL' ? { uFunctionId: string, pregion: string } :
     T extends 'UserManager' ? { username: string } :
     never;
 
