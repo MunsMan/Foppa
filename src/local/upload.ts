7c7a08bf3e3d43a8b2c3953f0f3290c4e8382c9d
MunsMan <munsman.github@gmail.com>
2023-08-18 12:35:38 +0200

    [form] just some formatting

--- src/local/upload.ts
+++ src/local/upload.ts
@@ -2,31 +2,28 @@ import { LambdaClient } from '@aws-sdk/client-lambda';
 import { S3Client } from '@aws-sdk/client-s3';
 import { fromIni } from '@aws-sdk/credential-provider-ini';
 import { uploadLambda } from '@libs/lambda';
-import { uploadCodeS3 } from '@libs/s3';
-
+import { localS3Upload } from '@libs/s3';
 
 const credentials = fromIni({ profile: 'foppa' });
 const clientConfig = {
     credentials,
     region: 'us-east-1',
-}
-const s3Client = new S3Client(clientConfig)
-const lambdaClient = new LambdaClient(clientConfig)
+};
+const s3Client = new S3Client(clientConfig);
+const lambdaClient = new LambdaClient(clientConfig);
 
-const username = 'test'
-const functionName = 'test'
-const bucket = 'foppa'
-const role = 'arn:aws:iam::807699729275:role/LabRole'
+const username = 'test';
+const functionName = 'test';
+const bucket = 'foppa';
+const role = 'arn:aws:iam::807699729275:role/LabRole';
 
 if (process.argv.length === 3) {
-    const file = process.argv[2]
-    uploadCodeS3(s3Client, functionName, file, bucket, username).then(
-        (response) => {
-            console.log(response);
-            uploadLambda(lambdaClient, functionName, role, username, 'handler.main');
-        })
-    console.log('lambda is uploaded!')
+    const file = process.argv[2];
+    localS3Upload(s3Client, functionName, file, bucket, username).then((response) => {
+        console.log(response);
+        uploadLambda(lambdaClient, functionName, role, username, 'handler.main');
+    });
+    console.log('lambda is uploaded!');
 } else {
-    console.log(process.argv)
+    console.log(process.argv);
 }
-
