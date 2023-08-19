25ff388b2fbdceb94ebbbbfd8e2dd175b8b0c455
MunsMan <munsman.github@gmail.com>
2023-08-19 12:37:32 +0200

    [auth] securing the entire website with auth

--- src/frontend/src/routes/login/+page.server.ts
+++ src/frontend/src/routes/login/+page.server.ts
@@ -1,45 +1,37 @@
 import { redirect, fail } from '@sveltejs/kit';
 import type { Actions } from './$types';
-
-import jwt from 'jsonwebtoken';
-
-const secret = 'test';
-
-const createSessionId = (username: string) =>
-	jwt.sign({ username }, secret, {
-		subject: username,
-		expiresIn: '30m',
-		audience: username
-	});
+import { createSessionId } from '$lib/auth';
 
 const hash = (password: string) => {
-	return password;
+    return password;
 };
 
 const getUser = async (username: string | undefined) => {
-	if (username === 'foppa') {
-		return {
-			password: 'abc',
-			username: 'foppa'
-		};
-	}
-	return undefined;
+    if (username === 'foppa') {
+        return {
+            password: 'abc',
+            username: 'foppa'
+        };
+    }
+    return undefined;
 };
 
 export const actions = {
-	default: async ({ cookies, request }) => {
-		const data = await request.formData();
-		const username = data.get('username')?.toString() ?? '';
-		const password = data.get('password')?.toString() ?? '';
-		const user = await getUser(username?.toString());
-		if (!user) {
-			return fail(400, { username, notFound: true });
-		}
-		if (user.password !== hash(password)) {
-			return fail(400, { username, wrong: true });
-		}
-		const sessionId = createSessionId(username);
-		cookies.set('sessionId', sessionId);
-		throw redirect(303, '/');
-	}
+    default: async ({ cookies, request }) => {
+        const url = new URL(request.url);
+        const source: string = url.searchParams.get('redirect') ?? '/';
+        const data = await request.formData();
+        const username = data.get('username')?.toString() ?? '';
+        const password = data.get('password')?.toString() ?? '';
+        const user = await getUser(username?.toString());
+        if (!user) {
+            return fail(400, { username, notFound: true });
+        }
+        if (user.password !== hash(password)) {
+            return fail(400, { username, wrong: true });
+        }
+        const sessionId = createSessionId(username);
+        cookies.set('sessionId', sessionId);
+        throw redirect(303, source);
+    }
 } satisfies Actions;
--- src/frontend/src/routes/login/+page.svelte
+++ src/frontend/src/routes/login/+page.svelte
@@ -2,12 +2,24 @@
 	import type { ActionData } from './$types';
 
 	export let form: ActionData;
-
-	$: console.log(form);
 </script>
 
 <div class="container">
 	<h1 class="text">Login</h1>
+	{#if form?.wrong}
+		<div class="row">
+			<div class="error">
+				<p>Your username or password was wrong!</p>
+			</div>
+		</div>
+	{/if}
+	{#if form?.notFound}
+		<div class="row">
+			<div class="error">
+				<p class="text">Unknown User<br />please register first</p>
+			</div>
+		</div>
+	{/if}
 	<form method="post">
 		<div class="row">
 			<label for="username">
@@ -21,7 +33,7 @@
 				<input id="password" type="password" name="password" />
 			</label>
 		</div>
-		<div class="row"><button>LogIn</button></div>
+		<div class="row"><button type="submit" class="button">Log In</button></div>
 	</form>
 </div>
 
@@ -31,11 +43,30 @@
 		flex-direction: column;
 	}
 	.row {
+		margin: 2vh;
 		display: flex;
 		flex-direction: row;
 		justify-content: space-around;
 	}
 	.text {
 		text-align: center;
+	}
+	.error {
+		border: solid 1px red;
+		border-radius: 5px;
+		background-color: rgba(200, 10, 10, 0.5);
+		color: whitesmoke;
+		width: 50%;
+	}
+	.button {
+		background-color: blue; /* Green */
+		border: none;
+		border-radius: 5px;
+		color: white;
+		padding: 15px 32px;
+		text-align: center;
+		text-decoration: none;
+		display: inline-block;
+		font-size: 16px;
 	}
 </style>
