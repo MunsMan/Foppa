79fb6bfbe0c46944ff4a4badaeb8c98b7cc16a70
MunsMan <munsman.github@gmail.com>
2023-08-19 12:37:07 +0200

    [auth] moving token handling to own file

---
+++ src/frontend/src/lib/auth/index.ts
@@ -0,0 +1,30 @@
+import jwt from 'jsonwebtoken';
+
+const secret = 'test';
+const issuer = 'foppa';
+
+export const createSessionId = (username: string) =>
+    jwt.sign({ username }, secret, {
+        subject: username,
+        expiresIn: '30m',
+        audience: username,
+        issuer: issuer
+    });
+
+export const verifySession = (token: string) => {
+    try {
+        const res = jwt.verify(token, secret, { issuer, complete: true });
+        let username = '';
+        if (typeof res.payload === 'string') {
+            username = JSON.parse(res.payload).username;
+        } else {
+            username = res.payload.username;
+        }
+        return {
+            valid: true,
+            username
+        };
+    } catch (err) {
+        return { valid: false };
+    }
+};
