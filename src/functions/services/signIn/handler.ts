e21cac70ce4c9fdebeb82f6227e109532007a1fc
MunsMan <munsman.github@gmail.com>
2023-08-25 15:41:10 +0200

    [signIn] securing the signIn with bcrypt in lambda and not svelte

--- src/functions/services/signIn/handler.ts
+++ src/functions/services/signIn/handler.ts
@@ -3,6 +3,7 @@ import { formatJSONResponse } from '@libs/api-gateway';
 import DynamoDB from '@libs/dynamodb';
 import { middyfy } from '@libs/lambda';
 import schema from './schema';
+import * as bycrypt from 'bcryptjs';
 
 const db = new DynamoDB();
 
@@ -14,9 +15,11 @@ const signInService: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (
         if (user.username === username) {
             return formatJSONResponse({ created: false, message: 'User already exists!' });
         }
-    } catch (error) { }
-    await db.putValue('UserManager', { username, password, functionCounter: 0 });
-    return formatJSONResponse({ created: true, message: 'User successfully created!' });
+    } catch (error) {
+        const hash = await bycrypt.hash(password, bycrypt.genSaltSync());
+        await db.putValue('UserManager', { username, password: hash, functionCounter: 0 });
+        return formatJSONResponse({ created: true, message: 'User successfully created!' });
+    }
 };
 
 export const main = middyfy(signInService, schema);
