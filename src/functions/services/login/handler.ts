8f35b75aeb3e0599f38489099738ae8481df1bd8
MunsMan <munsman.github@gmail.com>
2023-08-25 14:36:09 +0200

    [bcrypt] adding bcrypt compare for password validation

--- src/functions/services/login/handler.ts
+++ src/functions/services/login/handler.ts
@@ -3,20 +3,23 @@ import { formatJSONResponse } from '@libs/api-gateway';
 import DynamoDB from '@libs/dynamodb';
 import { middyfy } from '@libs/lambda';
 import schema from './schema';
+import * as bcrypt from 'bcrypt';
 
 const db = new DynamoDB();
 
 const loginService: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) => {
     const username = event.body.username;
     const password = event.body.password;
+    console.log(username, password);
     try {
         const user = await db.getValue('UserManager', { username });
-        if (password === user.password) {
-            return formatJSONResponse({ valid: true });
+        if (bcrypt.compareSync(password, user.password)) {
+            return formatJSONResponse({ status: 'valid', username });
         }
+        return formatJSONResponse({ status: 'wrong', username }, 404);
     } catch (error) {
         console.error(error);
-        return formatJSONResponse({ valid: false }, 404);
+        return formatJSONResponse({ username, status: 'notFound' }, 404);
     }
 };
 
