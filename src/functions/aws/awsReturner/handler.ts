57c0248619579a9f45fd8253ccb8dc0667b77394
MunsMan <munsman.github@gmail.com>
2023-09-08 01:02:23 +0200

    [awsReturner] upgrading to axios for conistency and adding some metadata like userFunctionLogs

--- src/functions/aws/awsReturner/handler.ts
+++ src/functions/aws/awsReturner/handler.ts
@@ -1,23 +1,24 @@
-import fetch from 'node-fetch';
+import { getExecutionLog } from '@libs/cloudwatch';
+import axios from 'axios';
 const SERVICE_URL = process.env.SERVICE_URL;
 
 const awsReturner = async (event: any) => {
-    const { uFunctionId, executionStart, executionId, pregion } = event.requestPayload.metadata;
+    const { uFunctionId, executionStart, executionId, pregion, functionName } =
+        event.requestPayload.metadata;
     const executionEnd = Date.now();
-    const response = await fetch(`${SERVICE_URL}/return/${uFunctionId}`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            result: event.responsePayload,
-            executionStart,
-            executionEnd,
-            executionId,
-            pregion,
-        })
-    })
-    console.log(response)
+    const userFunctionLogs = await getExecutionLog(
+        functionName,
+        event.requestContext.requestId,
+        executionStart
+    );
+    await axios.post(`${SERVICE_URL}/return/${uFunctionId}`, {
+        result: event.responsePayload,
+        executionStart,
+        executionEnd,
+        executionId,
+        pregion,
+        userFunctionLogs,
+    });
 };
 
 export const main = awsReturner;
