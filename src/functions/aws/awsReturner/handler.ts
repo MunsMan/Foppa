19c5e34dfd6abe3e72b248c106e48fd43ea00e86
MunsMan <munsman.github@gmail.com>
2023-09-12 20:02:56 +0200

    [awsReturner] updating to new getExecutionLog Version

--- src/functions/aws/awsReturner/handler.ts
+++ src/functions/aws/awsReturner/handler.ts
@@ -1,15 +1,15 @@
+import { CloudWatchLogsClient } from '@aws-sdk/client-cloudwatch-logs';
 import { getExecutionLog } from '@libs/cloudwatch';
 import { Context } from 'aws-lambda';
 import axios from 'axios';
 const SERVICE_URL = process.env.SERVICE_URL;
 
-const awsReturner = async (event: any) => {
-    const { uFunctionId, executionStart, executionId, pregion, functionName } =
 const awsReturner = async (event: any, context: Context) => {
     const { uFunctionId, executionStart, executionId, pregion, functionName, runnerRequestId } =
         event.requestPayload.metadata;
     const executionEnd = Date.now();
     const userFunctionLogs = await getExecutionLog(
+        new CloudWatchLogsClient({}),
         functionName,
         event.requestContext.requestId,
         executionStart
