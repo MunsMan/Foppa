341ad66311418cd8fee7524200f2b3caa0174ad2
MunsMan <munsman.github@gmail.com>
2023-09-14 11:16:16 +0200

    [local] finishing test script

--- src/local/concurrentcy_test.ts
+++ src/local/concurrentcy_test.ts
@@ -62,23 +62,8 @@ const pullRuntimeStatus = async (executionIds: number[]) => {
 };
 
 // @ts-ignore
-const saveData = async (outputData: StatusResponse[]) => {
-    const plotData = outputData.reduce((res, value) => {
-        res.push({
-            firstResponderStart: value.logs.firstResponder.logs.executionStart,
-            firstResponderEnd: value.logs.firstResponder.logs.executionEnd,
-            schedulerStart: value.logs.scheduler.logs.executionStart,
-            schedulerEnd: value.logs.scheduler.logs.executionEnd,
-            runnerStart: value.logs.runner.logs.executionStart,
-            runnerEnd: value.logs.runner.logs.executionEnd,
-            returnerStart: value.logs.returner.logs.executionStart,
-            returnerEnd: value.logs.returner.logs.executionEnd,
-            userExectutionStart: Number(value.logs.returner.awsWrapper.awsExecutionStart),
-            userExectutionEnd: Number(value.logs.returner.awsWrapper.awsExecutionEnd),
-        });
-        return res;
-    }, []);
-    await writeFile('executionTimes.json', JSON.stringify(plotData));
+const saveData = async (outputData: any) => {
+    await writeFile('executionTimes.json', JSON.stringify(outputData));
 };
 
 interface CloudWatchRequest {
@@ -112,7 +97,9 @@ const mapFunctionNames = {
 };
 
 type PromiseObject = {
-    [K in string]: Promise<CloudWatchResponse>;
+    [K in string]: Promise<{
+        [R in string]: CloudWatchResponse;
+    }>;
 };
 
 const pullFoppaRuntimeLogs = async (requests: CloudWatchRequest[]) => {
@@ -127,27 +114,54 @@ const pullFoppaRuntimeLogs = async (requests: CloudWatchRequest[]) => {
     }, new Map());
     const promises: PromiseObject = {};
     for (const [key, batch] of batches.entries()) {
-        const requestIds = batch.map((request) => request.requestId);
-        batch.sort((a, b) => a.executionStart - b.executionStart);
-        const region = batch[0].region;
-        const functionName = batch[0].functionName;
-        const startTime = new Date(batch[0].executionStart);
-        startTime.setMinutes(startTime.getMinutes() - 3);
-        const executionStart = startTime.getTime();
-        const promise = new Promise<CloudWatchResponse>((resolve) => {
-            pullLog(
-                mapFunctionNames[functionName] ?? functionName,
-                requestIds,
-                region,
-                executionStart
-            ).then((res) => {
-                resolve({
-                    requests: batch,
-                    response: res.data,
+        interface ReduceSet {
+            region: Region;
+            functionName: string;
+            executionStart: number;
+            requestIds: string[];
+        }
+        const regionRequests = batch
+            .reduce<ReduceSet[]>((res, value) => {
+                const entry = res.find((a) => a.region === value.region);
+                if (entry) {
+                    entry.requestIds.push(value.requestId);
+                    if (entry.executionStart > value.executionStart) {
+                        entry.executionStart = value.executionStart;
+                    }
+                    return res;
+                }
+                res.push({
+                    region: value.region,
+                    functionName: value.functionName,
+                    executionStart: value.executionStart,
+                    requestIds: [value.requestId],
+                });
+                return res;
+            }, [])
+            .map((request) => {
+                const startTime = new Date(request.executionStart);
+                startTime.setMinutes(startTime.getMinutes() - 3);
+                request.executionStart = startTime.getTime();
+                return request;
+            });
+        const regionPromise: { [R in string]: Promise<CloudWatchResponse> } = {};
+        regionRequests.forEach((request) => {
+            const promise = new Promise<CloudWatchResponse>((resolve) => {
+                pullLog(
+                    mapFunctionNames[request.functionName] ?? request.functionName,
+                    request.requestIds,
+                    request.region,
+                    request.executionStart
+                ).then((res) => {
+                    resolve({
+                        requests: batch,
+                        response: res.data,
+                    });
                 });
             });
+            regionPromise[request.region] = promise;
+            promises[key] = resolveObject(regionPromise);
         });
-        promises[key] = promise;
     }
     return resolveObject(promises);
 };
@@ -206,23 +220,41 @@ const main = async () => {
     ]);
     console.dir(requests, { depth: null });
     const cloudwatchlogs = await pullFoppaRuntimeLogs(requests.flat());
-    const logData = Object.entries(cloudwatchlogs).map(([key, value]) => {
-        const reports = value.response.logs.filter((log) => log.message.includes('REPORT'));
-        const durations = reports.map((report) =>
-            Number(
-                report.message
-                    .split('\t')
-                    .filter((text) => text.includes('Duration'))[0]
-                    .split(' ')[1]
-            )
-        );
+    const logData = Object.entries(cloudwatchlogs).map(([key, regionObjects]) => {
+        const regions = Object.entries(regionObjects).map<{
+            region: string;
+            durations: number[];
+            averageDuration: number;
+        }>(([region, value]) => {
+            const reports = value.response.logs.filter((log) => log.message.includes('REPORT'));
+            const starts = value.response.logs.filter((log) => log.message.includes('START'));
+            const durations = reports.map((report) =>
+                Number(
+                    report.message
+                        .split('\t')
+                        .filter((text) => text.includes('Duration'))[0]
+                        .split(' ')[1]
+                )
+            );
+            const executionStarts = starts.map((start) => start.timestamp);
+            return {
+                region,
+                durations,
+                averageDuration:
+                    durations.reduce((res, value) => res + value, 0) / durations.length,
+                executionStarts,
+            };
+        });
+        const durations = regions.map((region) => region.durations).flat();
         return {
             functionName: key,
             durations,
             averageDuration: durations.reduce((res, value) => res + value, 0) / durations.length,
+            regions,
         };
     });
     console.dir(logData, { depth: null });
+    await saveData(logData);
 };
 
 main();
