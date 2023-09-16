d7afbc3f5121b196a9cbb4fa56b9634ba8152df6
MunsMan <munsman.github@gmail.com>
2023-09-16 14:03:03 +0200

    [logs] updating the log functions and types

--- src/libs/s3.ts
+++ src/libs/s3.ts
@@ -13,17 +13,20 @@ type Bucket = 'foppa-logs';
 
 const s3 = new S3Client({});
 
-interface LogIdentifier {
-    username: string;
-    functionId: string;
-    executionId: string;
-}
-
-export const LogEventTypes = ['scheduler', 'runner', 'returner'] as const;
+export const LogEventTypes = ['firstResponder', 'scheduler', 'runner', 'returner'] as const;
 
 type LogEvent = (typeof LogEventTypes)[number];
 
-type Log<T extends LogEvent> = T extends 'scheduler'
+interface LogIdentifier<T extends LogEvent> {
+    username: string;
+    functionId: string;
+    executionId: string;
+    event?: T;
+}
+
+type Log<T extends LogEvent> = T extends 'firstResponder'
+    ? LogObject
+    : T extends 'scheduler'
     ? SchedulerLog
     : T extends 'runner'
     ? RunnerLog
@@ -31,36 +34,42 @@ type Log<T extends LogEvent> = T extends 'scheduler'
     ? ReturnerLog
     : never;
 
-export const appendLog = async <T extends LogEvent>(
-    bucket: Bucket,
-    logId: LogIdentifier,
-    event: T,
-    log: Log<T>
-) => {
-    const logfile = await getLog(bucket, logId);
-    switch (event) {
-        case 'returner':
-            logfile.returner = log as ReturnerLog;
-            break;
-        case 'runner':
-            logfile.runner = log as RunnerLog;
-            break;
-        case 'scheduler':
-            logfile.scheduler = log as SchedulerLog;
-            break;
-    }
-    return await putLog(bucket, logId, logfile);
-};
+type LogResponse<T extends LogEvent> = Log<T>;
 
-export const getLog = async (bucket: Bucket, logId: LogIdentifier) => {
-    const { username, functionId, executionId } = logId;
-    const key = `${username}/${functionId}/${executionId}.json`;
-    const file = await getFile(bucket, key);
-    if (file) {
-        const logfile: LogObject = JSON.parse(file);
-        return logfile;
+export const getLog = async <T extends LogEvent = 'firstResponder'>(
+    bucket: Bucket,
+    logId: LogIdentifier<T>
+): Promise<LogResponse<T>> => {
+    const { username, functionId, executionId, event } = logId;
+    if (event) {
+        const key = `${username}/${functionId}/${executionId}/${event}.json`;
+        const file = await getFile(bucket, key);
+        if (file) {
+            const logfile: Log<T> = JSON.parse(file);
+            return logfile;
+        }
+        return LogNotFound(logId);
     }
-    LogNotFound(logId);
+    const [firstResponderLogs, schedulerLogs, runnerLogs, returnerLogs] = await Promise.allSettled([
+        getLog(bucket, { ...logId, event: 'firstResponder' }),
+        getLog(bucket, { ...logId, event: 'scheduler' }),
+        getLog(bucket, { ...logId, event: 'runner' }),
+        getLog(bucket, { ...logId, event: 'returner' }),
+    ]);
+    if (firstResponderLogs.status !== 'fulfilled') {
+        return LogNotFound(logId);
+    }
+    const logObject = firstResponderLogs.value;
+    if (schedulerLogs.status === 'fulfilled') {
+        logObject.scheduler = schedulerLogs.value;
+    }
+    if (runnerLogs.status === 'fulfilled') {
+        logObject.runner = runnerLogs.value;
+    }
+    if (returnerLogs.status === 'fulfilled') {
+        logObject.returner = returnerLogs.value;
+    }
+    return logObject as Log<T>;
 };
 
 const getFile = async (bucket: Bucket, key: string): Promise<string | undefined> => {
@@ -86,9 +95,13 @@ export const getFileS3 = async (s3Client: S3Client, bucket: string, key: string)
     return Buffer.from(await response.Body.transformToByteArray());
 };
 
-export const putLog = async (bucket: Bucket, logId: LogIdentifier, log: LogObject) => {
-    const { username, functionId, executionId } = logId;
-    const key = `${username}/${functionId}/${executionId}.json`;
+export const putLog = async <T extends LogEvent>(
+    bucket: Bucket,
+    logId: Required<LogIdentifier<T>>,
+    log: Log<T>
+) => {
+    const { username, functionId, executionId, event } = logId;
+    const key = `${username}/${functionId}/${executionId}/${event}.json`;
     return await putFile(bucket, key, JSON.stringify(log));
 };
 
