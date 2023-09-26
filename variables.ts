5bce76f4c15e8619d442d5304837baafa6402682
MunsMan <munsman.github@gmail.com>
2023-09-26 08:58:13 +0200

    [variables] adding a screte to the variables for authentication

--- variables.ts
+++ variables.ts
@@ -6,6 +6,7 @@ const variables = {
     LOGGING_BUCKET_NAME: 'foppa-logs',
     FOPPA_BUCKET_NAME: 'foppa',
     SERVICE_URL: 'https://bffa3z498i.execute-api.eu-central-1.amazonaws.com',
+    PRIVATE_KEY: process.env.PRIVATE_KEY,
 } as const;
 
 export default variables;
